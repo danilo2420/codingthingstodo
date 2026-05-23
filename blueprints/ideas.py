75cde1a0ac2056cfb06bb9b2496d3e6fcef80ecb
danilo <danimoraga1997@gmail.com>
2026-05-23 13:23:43 +0200

    Add authentication

--- blueprints/ideas.py
+++ blueprints/ideas.py
@@ -1,11 +1,13 @@
-from flask import Blueprint, make_response, jsonify
-from database import getIdeasCollection, docsToArray
+from flask import Blueprint, make_response, jsonify, request
+from database import getIdeasCollection, docsToArray, protected
 
 ideas_bp = Blueprint("ideas", __name__, url_prefix="/ideas")
 
 @ideas_bp.route("/list")
+@protected
 def listIdeas():
     collection = getIdeasCollection()
+
     if collection is None:
         return make_response({"error": "Collection could not be loaded"}, 500)
     return jsonify(docsToArray(collection.find()))
--- database.py
+++ database.py
@@ -2,6 +2,7 @@ from pymongo import MongoClient
 import os
 import sys
 from dotenv import load_dotenv
+from flask import request, jsonify
 
 if "--test" in sys.argv:
     load_dotenv(override=True)
@@ -10,6 +11,7 @@ MONGO_USER = os.getenv("MONGO_USER")
 MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
 MONGO_DOMAIN = os.getenv("MONGO_DOMAIN")
 MONGO_PORT = os.getenv("MONGO_PORT")
+DANI_AUTH = os.getenv("DANI_AUTH")
 
 URL = f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_DOMAIN}{MONGO_PORT}"
 print("URL IS " + URL)
@@ -24,7 +26,17 @@ def testDatabaseConnection():
     except:
         print("Database connection failed")
         return False
-    
+
+def protected(fn):
+    def wrapper(*args, **kwargs):
+        try:
+            auth_header = request.headers["Authorization"]
+            if auth_header != DANI_AUTH:
+                return jsonify({"error": "authentication failed"}, 400)
+            return fn(*args, **kwargs)
+        except Exception as e:
+            return jsonify({"error": "authentication failed"}, 400)
+    return wrapper
 
 def getIdeasCollection():
     try:
